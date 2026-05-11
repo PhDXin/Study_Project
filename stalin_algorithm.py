d516c330246321a270c8a33e8a8158807dd6bd15
PhDXin <160125280+PhDXin@users.noreply.github.com>
2026-05-11 11:36:56 +0800

    Add files via upload

--- stalin_algorithm.py
+++ stalin_algorithm.py
@@ -1,10 +1,15 @@
-l=[2,1,2,3,5]
+l=[2,1,2,3,5,10,7,11,13,15,7,2,4,20,1,32,64]
 def purge(l):
-	length=len(l)
+	counter=1
 	l_purged=[]
-	for i in range(1,length):
-		if l[i]>l[i-1]:
-			l_purged.append(l[i])
+	l_purged.append(l[0])
+	while l_purged != l:
+		if l[counter]>l[counter-1]:
+			l_purged.append(l[counter])
+			counter+=1
+		else:
+			l.pop(counter)
+		print("l:",l)
 	return l_purged
 l_purged=purge(l)
 print(l_purged)
